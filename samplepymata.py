254282a88dc525de39d559993773b7011097e04f
HAWKS Engineering <51273280+hawkseng@users.noreply.github.com>
2019-06-07 23:19:54 -0500

    Update samplepymata.py

--- samplepymata.py
+++ samplepymata.py
@@ -2,14 +2,10 @@ from pymata_aio.pymata3 import PyMata3
 from pymata_aio.constants import Constants
 from time import sleep
 
-arduino = PyMata3() # Why don't we do this?  I assume we use the open socket stuff...
-arduino.i2c_config() # or this?
+#arduino = PyMata3() # Why don't we do this?  I assume we use the open socket stuff...
+#arduino.i2c_config() # or this?
 #open socket for control board - this is from Matt
 board = PyMata3(ip_address = '192.168.0.177', ip_port=3030, ip_handshake='')
-
-# Models - What are these?!?  The two sensors on board?
-MODEL_02BA = 0
-MODEL_30BA = 1
 
 class TSYS01(object): # What is the purpose of making a "class?"
     
@@ -31,16 +27,16 @@ class TSYS01(object): # What is the purpose of making a "class?"
             self._temperature = 0
         
     def init(self):
-        if self._board is None:
+        if board is None:
             "No board!"
             return False
 
-        board.i2c_write_request(self._TSYS01_ADDR, [self._TSYS01_RESET])
+        board.i2c_write_request(_TSYS01_ADDR, [_TSYS01_RESET])
         
         # Wait for reset to complete
         sleep(0.01)
         
-        self._C = []
+        _C = []
         
         # Read calibration values and CRC
         for i in range(7):
@@ -73,7 +69,7 @@ class TSYS01(object): # What is the purpose of making a "class?"
         return True
         
         # Request D1 conversion (temperature)
-        self._board.i2c_write_request(self._MS5837_ADDR, [self._MS5837_CONVERT_D1_256 + 2*oversampling])
+        board.i2c_write_request(_TSYS01_ADDR, [_TSYS01_CONVERT_D1_256 + 2*oversampling])
     
         # Maximum conversion time increases linearly with oversampling
         # max time (seconds) ~= 2.2e-6(x) where x = OSR = (2^8, 2^9, ..., 2^13)
@@ -102,23 +98,16 @@ class TSYS01(object): # What is the purpose of making a "class?"
 
         self._D2 = d[0] << 16 | d[1] << 8 | d[2]
 
-        # Calculate compensated pressure and temperature
+        # Calculate temperature
         # using raw ADC values and internal calibration
         self._calculate()
         
         return True
-    
-    def setFluidDensity(self, denisty):
-        self._fluidDensity = denisty
-            
+
     # Temperature in requested units
     # default degrees C
     def temperature(self, conversion=UNITS_Centigrade):
         degC = self._temperature / 100.0
-        if conversion == UNITS_Farenheit:
-            return (9/5) * degC + 32
-        elif conversion == UNITS_Kelvin:
-            return degC - 273
         return degC
         
     # Cribbed from datasheet
@@ -127,17 +116,7 @@ class TSYS01(object): # What is the purpose of making a "class?"
         SENSi = 0
         Ti = 0
 
-        dT = self._D2-self._C[5]*256
-        if self._model == MODEL_02BA:
-            SENS = self._C[1]*65536+(self._C[3]*dT)/128
-            OFF = self._C[2]*131072+(self._C[4]*dT)/64
-            self._pressure = (self._D1*SENS/(2097152)-OFF)/(32768)
-        else:
-            SENS = self._C[1]*32768+(self._C[3]*dT)/256
-            OFF = self._C[2]*65536+(self._C[4]*dT)/128
-            self._pressure = (self._D1*SENS/(2097152)-OFF)/(8192)
-        
-        self._temperature = 2000+dT*self._C[6]/8388608
+       self._temperature = 2000+dT*self._C[6]/8388608
 
         # Second order compensation
         if self._model == MODEL_02BA:
@@ -164,38 +143,12 @@ class TSYS01(object): # What is the purpose of making a "class?"
         
         if self._model == MODEL_02BA:
             self._temperature = (self._temperature-Ti)
-            self._pressure = (((self._D1*SENS2)/2097152-OFF2)/32768)/100.0
+            
         else:
             self._temperature = (self._temperature-Ti)
-            self._pressure = (((self._D1*SENS2)/2097152-OFF2)/8192)/10.0   
-        
-    # Cribbed from datasheet
-    def _crc4(self, n_prom):
-        n_rem = 0
-        
-        n_prom[0] = ((n_prom[0]) & 0x0FFF)
-        n_prom.append(0)
-    
-        for i in range(16):
-            if i%2 == 1:
-                n_rem ^= ((n_prom[i>>1]) & 0x00FF)
-            else:
-                n_rem ^= (n_prom[i>>1] >> 8)
-                
-            for n_bit in range(8,0,-1):
-                if n_rem & 0x8000:
-                    n_rem = (n_rem << 1) ^ 0x3000
-                else:
-                    n_rem = (n_rem << 1)
-
-        n_rem = ((n_rem >> 12) & 0x000F)
-        
-        self.n_prom = n_prom
-        self.n_rem = n_rem
-    
-        return n_rem ^ 0x00
-    
+             
+           
 class TSYS01(TSYS01):
     def __init__(self):
-        TSYS01.__init__(self, MODEL_30BA) #What is this doing???
+        TSYS01.__init__(self, 1) #What is this doing???
        
