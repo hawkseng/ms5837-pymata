caa76b344bec8edf6a8b4ea696b61f23894a7533
HAWKS Engineering <51273280+hawkseng@users.noreply.github.com>
2019-06-08 23:18:04 -0500

    Update HAWKS-ms5837-pymata.py

--- HAWKS-ms5837-pymata.py
+++ HAWKS-ms5837-pymata.py
@@ -1,9 +1,12 @@
+# Import Modules
 from pymata_aio.pymata3 import PyMata3
 from pymata_aio.constants import Constants
 from time import sleep
 
-arduino = PyMata3()
-arduino.i2c_config()
+#open socket for control board
+board = PyMata3(ip_address = '192.168.0.177', ip_port=3030, ip_handshake='')
+#arduino = PyMata3()
+#arduino.i2c_config()
 
 # Models
 MODEL_02BA = 0
@@ -66,7 +69,7 @@ class MS5837(object):
             "No board!"
             return False
 
-        self._board.i2c_write_request(self._MS5837_ADDR, [self._MS5837_RESET])
+        board.i2c_write_request(_MS5837_ADDR, [_MS5837_RESET])
         
         # Wait for reset to complete
         sleep(0.01)
@@ -76,9 +79,9 @@ class MS5837(object):
         # Read calibration values and CRC
         for i in range(7):
             c = []
-            self._board.i2c_read_request(self._MS5837_ADDR, self._MS5837_PROM_READ + (2*i), 2, Constants.I2C_READ)
-            self._board.sleep(0.1)
-            data = self._board.i2c_read_data(self._MS5837_ADDR)
+            board.i2c_read_request(_MS5837_ADDR, _MS5837_PROM_READ + (2*i), 2, Constants.I2C_READ)
+            board.sleep(0.1)
+            data = board.i2c_read_data(_MS5837_ADDR)
             for j in range(len(data)):
                 c.append(hex(data[j])[2:])
                 #print(str(hex(data[j])))
@@ -104,7 +107,7 @@ class MS5837(object):
         return True
         
     def read(self, oversampling=OSR_8192):
-        if self._board is None:
+        if board is None:
             print("No board!")
             return False
         
@@ -113,7 +116,7 @@ class MS5837(object):
             return False
         
         # Request D1 conversion (temperature)
-        self._board.i2c_write_request(self._MS5837_ADDR, [self._MS5837_CONVERT_D1_256 + 2*oversampling])
+        board.i2c_write_request(_MS5837_ADDR, [_MS5837_CONVERT_D1_256 + 2*oversampling])
     
         # Maximum conversion time increases linearly with oversampling
         # max time (seconds) ~= 2.2e-6(x) where x = OSR = (2^8, 2^9, ..., 2^13)
@@ -121,24 +124,24 @@ class MS5837(object):
         sleep(2.5e-6 * 2**(8+oversampling))
         
         #d = self._bus.read_i2c_block_data(self._MS5837_ADDR, self._MS5837_ADC_READ, 3)
-        self._board.i2c_read_request(self._MS5837_ADDR, self._MS5837_ADC_READ, 3, Constants.I2C_READ)
-        self._board.sleep(0.1)
-        d = self._board.i2c_read_data(self._MS5837_ADDR)
+        board.i2c_read_request(_MS5837_ADDR, _MS5837_ADC_READ, 3, Constants.I2C_READ)
+        board.sleep(0.1)
+        d = board.i2c_read_data(_MS5837_ADDR)
 
 
         self._D1 = d[0] << 16 | d[1] << 8 | d[2]
         
         # Request D2 conversion (pressure)
-        self._board.i2c_write_request(self._MS5837_ADDR, [self._MS5837_CONVERT_D2_256 + 2*oversampling])
+        board.i2c_write_request(_MS5837_ADDR, [_MS5837_CONVERT_D2_256 + 2*oversampling])
         
     
         # As above
         sleep(2.5e-6 * 2**(8+oversampling))
  
         #d = self._bus.read_i2c_block_data(self._MS5837_ADDR, self._MS5837_ADC_READ, 3)
-        self._board.i2c_read_request(self._MS5837_ADDR, self._MS5837_ADC_READ, 3, Constants.I2C_READ)
-        self._board.sleep(0.1)
-        d = self._board.i2c_read_data(self._MS5837_ADDR)
+        board.i2c_read_request(_MS5837_ADDR, _MS5837_ADC_READ, 3, Constants.I2C_READ)
+        board.sleep(0.1)
+        d = board.i2c_read_data(_MS5837_ADDR)
 
         self._D2 = d[0] << 16 | d[1] << 8 | d[2]
 
